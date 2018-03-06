474ae3882439c04adf8f4e745db2111d988e391e
wang <jjclftjd@gmail.com>
2018-03-06 22:36:41 +0900

    examfiles

--- FizzBizz.py
+++ FizzBizz.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def fizzbizz(num):
-    if num % 3 == 0 and num != 0:
-        return 'Fizz'
-    elif num % 5 ==0 and num != 0:
-        return 'Bizz'
+    if num >=1 and num <= 100:
+        if num % 3 == 0 and num != 0:
+            return 'Fizz'
+        elif num % 5 ==0 and num != 0:
+            return 'Bizz' 
     else:
         return 'ValueError'
     
